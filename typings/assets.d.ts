9ae293d65690697bae50255e161892f76de33f2f
Rasmus Eneman <rasmus.eneman@learningwell.se>
2018-11-09 16:45:52 +0100

    Initial commit from Create React App

---
+++ typings/assets.d.ts
@@ -0,0 +1,12 @@
+declare module '*.css';
+declare module '*.sass';
+declare module '*.scss';
+declare module '*.gif';
+declare module '*.jpeg';
+declare module '*.jpg';
+declare module '*.json';
+declare module '*.mp3';
+declare module '*.mp4';
+declare module '*.png';
+declare module '*.svg';
+declare module '*.webm';
