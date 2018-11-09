d726185f19d996d6e3308cdf0415f9f92789be18
Axel Vestin <avn@learningwell.se>
2018-11-09 23:46:22 +0100

    Adds images and imageurl to products

--- backend/dbSchema.sql
+++ backend/dbSchema.sql
@@ -10,6 +10,8 @@ CREATE TABLE product (
     productId SERIAL PRIMARY KEY,
     name varchar(80) NOT NULL,
     score int NOT NULL,
+    img varchar(80) NOT NULL,
+
     manufacturerId int references manufacturer(manufacturerId)
 );
 
@@ -19,6 +21,6 @@ VALUES('LearningWell'),
       ('StarMeat Inc');
 
 INSERT INTO product(name, score, manufacturerId)
-VALUES('Mjölk', 1, (SELECT manufacturerId FROM manufacturer WHERE manufacturer.name='LearningWell')),
-      ('Köttfärs', 2, (SELECT manufacturerId FROM manufacturer WHERE manufacturer.name='MachineFood')),
-      ('Godis', 3, (SELECT manufacturerId FROM manufacturer WHERE manufacturer.name='StarMeat Inc'));
+VALUES('Mjölk', 1, (SELECT manufacturerId FROM manufacturer WHERE manufacturer.name='LearningWell'), 'products/images/mjolk.jpg'),
+      ('Köttfärs', 2, (SELECT manufacturerId FROM manufacturer WHERE manufacturer.name='MachineFood'), 'products/images/kott.jpg'),
+      ('Godis', 3, (SELECT manufacturerId FROM manufacturer WHERE manufacturer.name='StarMeat Inc')), 'products/images/godis.jpg';
