805e4ba44e1589de6fcca6d455f8eb7fa841404f
Rasmus Eneman <rasmus.eneman@learningwell.se>
2018-11-10 12:18:35 +0100

    improve basket

--- frontend/src/pages/BasketPage.tsx
+++ frontend/src/pages/BasketPage.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { List, IconButton } from '@material-ui/core'
+import { List, IconButton, Typography } from '@material-ui/core'
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket'
 import RemoveShoppingCartIcon from '@material-ui/icons/RemoveShoppingCart'
-import { Product } from '../entities'
+import { Product, TreeIcon } from '../entities'
 import { Navigation } from '../ui/Navigation'
 import { ProductListItem } from '../ui/ProductListItem'
 
@@ -33,21 +34,65 @@ export const BasketPage = (props: {
           overflow: 'auto',
         }}
       >
-        <div style={{ flex: 1 }}>
-          <List>
-            {props.basket.map((product, i) => (
-              <ProductListItem
-                key={i}
-                product={product}
-                divider={i < props.basket.length - 1}
-                actionButton={
-                  <IconButton onClick={() => props.removeFromBasket(product)}>
-                    <RemoveShoppingCartIcon />
-                  </IconButton>
-                }
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: props.basket.length > 0 ? '' : 'center',
+            justifyContent: props.basket.length > 0 ? '' : 'center',
+            flex: 1,
+          }}
+        >
+          {props.basket.length > 0 ? (
+            <>
+              <div style={{ padding: 16, paddingTop: 24, paddingBottom: 8 }}>
+                <Typography variant="h5">Din kundkorg</Typography>
+                <div>
+                  {Array.from({ length: 3 }).map((_, idx) => (
+                    <TreeIcon key={idx} />
+                  ))}
+                  {Array.from({ length: 5 - 3 }).map((_, idx) => (
+                    <TreeIcon key={idx} grey />
+                  ))}
+                </div>
+              </div>
+
+              <List style={{ width: '100%' }}>
+                {props.basket.map((product, i) => (
+                  <ProductListItem
+                    key={i}
+                    product={product}
+                    divider={i < props.basket.length - 1}
+                    actionButton={
+                      <IconButton
+                        onClick={() => props.removeFromBasket(product)}
+                      >
+                        <RemoveShoppingCartIcon />
+                      </IconButton>
+                    }
+                  />
+                ))}
+              </List>
+            </>
+          ) : (
+            <div
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                flexDirection: 'column',
+              }}
+            >
+              <ShoppingBasketIcon
+                style={{ fontSize: 92 }}
+                color="disabled"
+                fontSize="large"
               />
-            ))}
-          </List>
+              <Typography variant="body1" style={{ maxWidth: '80%' }}>
+                Add products to your shopping basket from the search page
+              </Typography>
+            </div>
+          )}
         </div>
       </div>
       <Navigation />
